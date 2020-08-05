d4b2d6d8d2f9dc22124dc360817be6ef15e37be0
hmdrzaa11 <hmdrzaa11@gmail.com>
2020-08-06 03:18:04 +0430

    added db connection

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 let express = require("express");
 let dotenv = require("dotenv");
 let path = require("path");
-
-//db connections
-let sequelize = require("./db/connection");
-//Tour models
-let Tour = require("./models/Tour");
-let TourImages = require("./models/TourImages");
-let TourStartDates = require("./models/TourStartDates");
-let TourStartLocation = require("./models/TourStartLocation");
 let app = express();
-
-//.env config
-dotenv.config({
-  path: path.join(__dirname, ".env"),
-});
+dotenv.config({ path: path.join(__dirname, ".env") });
+let sequelize = require("./db/connection");
 
 let tourRouter = require("./routes/tourRoutes");
 
 //api v1
 app.use("/api/v1/tours", tourRouter);
 
-//sync models
-sequelize.sync();
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => console.log(`on port ${PORT}`));
